c918ed9d4ba7d7100ebd501e54bcaba7a8cfd533
Kali113 <kali113xd@gmail.com>
2014-10-26 16:14:37 +0200

    first

---
+++ CemeteryManager/src/Model/Inspector.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class Inspector extends User {
+	public Inspector(){
+		
+	}
+
+}
---
+++ CemeteryManager/src/Model/User.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class User {
+	private int id;
+	private String name;
+	public User(){
+		id=-1;
+		name="";
+	}
+	public User(int id, String name){
+		this.id=id;
+		this.name=name;
+	}
+
+}
