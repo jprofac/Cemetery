f1545ef2ec12d717668e67ab3979230434521971
David <David@David-pc>
2014-12-01 17:27:36 +0200

    updated cemeteryRepo, requestRepo

--- CemeteryManager/src/Repository/CemeteryRepo.java
+++ CemeteryManager/src/Repository/CemeteryRepo.java
@@ -1,14 +1,120 @@
 package Repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import Model.Cemetery;
 
+import com.mysql.jdbc.Statement;
+
 public class CemeteryRepo {
-	public List<Cemetery> getAllCemetery(){return null;}
-	public void addCemetery(Cemetery g){}
-	public void updateCemetery(Cemetery g){}
-	public void deleteCemetery(Cemetery g){}
-	public Cemetery getCemeteryById(int id){return null;}
-	public List<Cemetery> searchCemetery(String s){return null;}
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	private List<Cemetery> allCemetery = null;
+
+	public CemeteryRepo() throws ClassNotFoundException, SQLException {
+		connectToDB();
+		getAllCemetery();
+	}
+
+	public List<Cemetery> getAllCemetery() throws SQLException {
+		allCemetery = new ArrayList<Cemetery>();
+		resultSet = statement.executeQuery("select * from cemetery");
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			String name = resultSet.getString("name");
+			String address = resultSet.getString("address");
+			Cemetery newCemetery = new Cemetery(id, name, address);
+			allCemetery.add(newCemetery);
+		}
+		return allCemetery;
+	}
+
+	public void addCemetery(Cemetery g) throws SQLException {
+		if (getCemeteryById(g.getId()) == null
+				&& isValid(g.getName(), g.getAddress())) {
+			preparedStatement = connect
+					.prepareStatement("insert into  cemetery values (? , ?, ?)");
+			preparedStatement.setInt(1, g.getId());
+			preparedStatement.setString(2, g.getName());
+			preparedStatement.setString(3, g.getAddress());
+			preparedStatement.executeUpdate();
+			allCemetery.add(g);
+		}
+	}
+
+	public void updateCemetery(Cemetery g) throws SQLException {
+		if (isValid(g.getName(), g.getAddress())) {
+			preparedStatement = connect
+					.prepareStatement("update cemetery set name = ?, address = ? where id = ?");
+			preparedStatement.setString(1, g.getName());
+			preparedStatement.setString(2, g.getAddress());
+			preparedStatement.setInt(3, g.getId());
+			preparedStatement.executeUpdate();
+
+			for (Cemetery cemetery : allCemetery) {
+				if (cemetery.getId() == g.getId()) {
+					cemetery.setName(g.getName());
+					cemetery.setAddress(g.getAddress());
+				}
+			}
+		}
+	}
+
+	public void deleteCemetery(Cemetery g) throws SQLException {
+		if (getCemeteryById(g.getId()) != null) {
+			preparedStatement = connect
+					.prepareStatement("delete from cemetery where id = ?");
+			preparedStatement.setInt(1, g.getId());
+			preparedStatement.executeUpdate();
+			allCemetery.remove(g);
+		}
+
+	}
+
+	public Cemetery getCemeteryById(int id) {
+		for (Cemetery cemetery : allCemetery) {
+			if (cemetery.getId() == id) {
+				return cemetery;
+			}
+		}
+		return null;
+	}
+
+	public List<Cemetery> searchCemetery(String s) {
+		List<Cemetery> searchResult = new ArrayList<Cemetery>();
+		for (Cemetery cemetery : allCemetery) {
+			if (cemetery.getName().contains(s)
+					|| cemetery.getAddress().contains(s)) {
+				searchResult.add(cemetery);
+			}
+		}
+		return searchResult;
+	}
+
+	private boolean isValid(String name, String address) {
+		if (name.matches("[a-zA-Z]+") && address.matches("[a-zA-z0-9.,]+")) {
+			return true;
+		}
+		return false;
+	}
+
+	private void connectToDB() throws ClassNotFoundException, SQLException {
+		// this will load the MySQL driver, each DB has its own driver
+		Class.forName("com.mysql.jdbc.Driver");
+		// setup the connection with the DB.
+		connect = DriverManager
+				.getConnection("jdbc:mysql://localhost/proiectcolectiv?"
+						+ "user=root&password=root");
+
+		// statements allow to issue SQL queries to the database
+		statement = (Statement) connect.createStatement();
+	}
 }
--- CemeteryManager/src/Repository/RequestRepo.java
+++ CemeteryManager/src/Repository/RequestRepo.java
@@ -1,14 +1,117 @@
 package Repository;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import Model.Request;
 
+import com.mysql.jdbc.Statement;
+
 public class RequestRepo {
-	public List<Request> getAllRequest(){return null;}
-	public void addRequest(Request g){}
-	public void updateRequest(Request g){}
-	public void deleteRequest(Request g){}
-	public Request getRequestById(int id){return null;}
-	public List<Request> searchRequest(String s){return null;}
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	private List<Request> allRequest = null;
+
+	public RequestRepo() throws ClassNotFoundException, SQLException {
+		connectToDB();
+		getAllRequest();
+	}
+
+	public List<Request> getAllRequest() throws SQLException {
+		allRequest = new ArrayList<Request>();
+		resultSet = statement.executeQuery("select * from request");
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			Date date = resultSet.getDate("date");
+			int infocet = resultSet.getInt("infocet");
+			boolean completed = resultSet.getBoolean("completed");
+			Request newRequest = new Request(id, date, infocet, completed);
+			allRequest.add(newRequest);
+		}
+		return allRequest;
+	}
+
+	public void addRequest(Request g) throws SQLException {
+		if (getRequestById(g.getId()) == null) {
+			preparedStatement = connect
+					.prepareStatement("insert into  request values (?, ?, ?, ?)");
+			preparedStatement.setInt(1, g.getId());
+			preparedStatement.setDate(2, (Date) g.getDate());
+			preparedStatement.setInt(3, g.getInfocet());
+			preparedStatement.setBoolean(4, g.isCompleted());
+			preparedStatement.executeUpdate();
+			allRequest.add(g);
+		}
+	}
+
+	public void updateRequest(Request g) throws SQLException {
+		if (getRequestById(g.getId()) != null) {
+			preparedStatement = connect
+					.prepareStatement("update request set date = ?, infocet = ?, completed = ? where id = ?");
+			preparedStatement.setDate(1, (Date) g.getDate());
+			preparedStatement.setInt(2, g.getInfocet());
+			preparedStatement.setBoolean(3, g.isCompleted());
+			preparedStatement.setInt(4, g.getId());
+			preparedStatement.executeUpdate();
+
+			for (Request request : allRequest) {
+				if (request.getId() == g.getId()) {
+					request.setDate(g.getDate());
+					request.setInfocet(g.getInfocet());
+					request.setCompleted(g.isCompleted());
+				}
+			}
+		}
+	}
+
+	public void deleteRequest(Request g) throws SQLException {
+		if (getRequestById(g.getId()) != null) {
+			preparedStatement = connect
+					.prepareStatement("delete from request where id = ?");
+			preparedStatement.setInt(1, g.getId());
+			preparedStatement.executeUpdate();
+			allRequest.remove(g);
+		}
+	}
+
+	public Request getRequestById(int id) {
+		for (Request request : allRequest) {
+			if (request.getId() == id) {
+				return request;
+			}
+		}
+		return null;
+	}
+
+	public List<Request> searchRequest(String s) {
+		List<Request> searchResult = new ArrayList<Request>();
+		for (Request request : allRequest) {
+			String infocet = Integer.toString(request.getInfocet());
+			String date = request.getDate().toString();
+			if (date.contains(s) || infocet.contains(s)) {
+				searchResult.add(request);
+			}
+		}
+		return searchResult;
+	}
+
+	private void connectToDB() throws ClassNotFoundException, SQLException {
+		// this will load the MySQL driver, each DB has its own driver
+		Class.forName("com.mysql.jdbc.Driver");
+		// setup the connection with the DB.
+		connect = DriverManager
+				.getConnection("jdbc:mysql://localhost/proiectcolectiv?"
+						+ "user=root&password=root");
+
+		// statements allow to issue SQL queries to the database
+		statement = (Statement) connect.createStatement();
+	}
 }
