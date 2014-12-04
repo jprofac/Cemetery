6c260c841db93564da0843f111ce648869c5f5d5
David <David@David-pc>
2014-12-04 10:59:28 +0200

    ..

--- CemeteryManager/src/Repository/RequestRepo.java
+++ CemeteryManager/src/Repository/RequestRepo.java
@@ -1,30 +1,15 @@
 package Repository;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import Model.Cemetery;
 import Model.Data;
 import Model.Request;
 
-import com.mysql.jdbc.Statement;
-
 public class RequestRepo {
-	private Connection connect = null;
-	private Statement statement = null;
-	private PreparedStatement preparedStatement = null;
-	private ResultSet resultSet = null;
-	private List<Request> allRequest = null;
-
-	public RequestRepo() throws ClassNotFoundException, SQLException {
-		connectToDB();
-		getAllRequest();
+	
+	public RequestRepo() {
 	}
 
 	public List<Request> getAllRequest() throws SQLException {
@@ -61,17 +46,5 @@ public class RequestRepo {
 			}
 		}*/
 		return null;
-	}
-
-	private void connectToDB() throws ClassNotFoundException, SQLException {
-		// this will load the MySQL driver, each DB has its own driver
-		Class.forName("com.mysql.jdbc.Driver");
-		// setup the connection with the DB.
-		connect = DriverManager
-				.getConnection("jdbc:mysql://localhost/proiectcolectiv?"
-						+ "user=root&password=root");
-
-		// statements allow to issue SQL queries to the database
-		statement = (Statement) connect.createStatement();
 	}
 }
