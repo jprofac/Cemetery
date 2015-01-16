e206e10394a1660d86f29cd1f9ea43d6502894da
Kali113 <kali113xd@gmail.com>
2015-01-16 22:02:05 +0200

    sdefsdf

--- CemeteryManager/src/procol.sql
+++ CemeteryManager/src/procol.sql
@@ -6,6 +6,7 @@ CREATE TABLE `Users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
